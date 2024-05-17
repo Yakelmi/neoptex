65a18fb93eba0245fe9e638ae83f96bd120c1399
Ilya ~ <162040889+Yakelmi@users.noreply.github.com>
2024-05-18 06:57:21 +0700

    🧸

---
+++ запуск для линкса/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing required Python packages..."
+pip install -r requirements.txt
+echo "Installation complete."
+read -p "Press [Enter] key to continue..."
---
+++ запуск для линкса/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting your Python project..."
+python main.py
+echo "Project execution complete."
+read -p "Press [Enter] key to continue..."
